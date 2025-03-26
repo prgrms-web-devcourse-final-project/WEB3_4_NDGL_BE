8439bcd724d3bb0567f5a41e23463a9be39bc456
han <tkrhktkdwk10@gmail.com>
2025-03-26 11:02:57 +0900

    chore: 코드 리팩토링, 기타 메서드 추가

--- src/main/java/com/ndgl/spotfinder/global/exception/ErrorCode.java
+++ src/main/java/com/ndgl/spotfinder/global/exception/ErrorCode.java
@@ -12,7 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 public enum ErrorCode {
 	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "로그인이 필요합니다."),
 
-	NOT_FOUND(HttpStatus.NOT_FOUND, "Not Found");
+	// LIKE
+	UNSUPPORTED_TARGET_TYPE(HttpStatus.NOT_FOUND, "지원하지 않는 대상 유형입니다");
 
 	private final HttpStatus httpStatus;
 	private final String message;
