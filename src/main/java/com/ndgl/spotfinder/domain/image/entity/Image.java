39493ff0aa37370c15cde276286c49cc50d4f03f
han <tkrhktkdwk10@gmail.com>
2025-03-27 11:02:25 +0900

    feat : add image domain

---
+++ src/main/java/com/ndgl/spotfinder/domain/image/entity/Image.java
@@ -0,0 +1,37 @@
+package com.ndgl.spotfinder.domain.image.entity;
+
+import com.ndgl.spotfinder.global.base.BaseTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Image extends BaseTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(nullable = false)
+	private long postId;
+
+	// TODO
+	// @OneToOne
+	// private Post post
+
+	@Column(nullable = false)
+	private String url;
+}
