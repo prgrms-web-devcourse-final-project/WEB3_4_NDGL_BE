c53d8c8556e773036df4dd3a22c57daa3f18a5d9
han <tkrhktkdwk10@gmail.com>
2025-03-27 11:50:10 +0900

    feat : add S3Config #18
    # Conflicts:
    #	.gitignore
    #	src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java
    
    # Conflicts:
    #	src/main/resources/application-dev.yml

---
+++ src/main/java/com/ndgl/spotfinder/global/aws/s3/S3Config.java
@@ -0,0 +1,45 @@
+package com.ndgl.spotfinder.global.aws.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class S3Config {
+
+	@Value("${spring.cloud.aws.credentials.access-key}")
+	private String accessKey;
+
+	@Value("${spring.cloud.aws.credentials.secret-key}")
+	private String secretKey;
+
+	@Bean
+	public AwsCredentialsProvider awsCredentialsProvider() {
+		return StaticCredentialsProvider.create(
+			AwsBasicCredentials.create(accessKey, secretKey)
+		);
+	}
+
+	@Bean
+	public S3Presigner s3Presigner(AwsCredentialsProvider credentialsProvider) {
+		return S3Presigner.builder()
+			.credentialsProvider(credentialsProvider)
+			.region(Region.AP_NORTHEAST_2)
+			.build();
+	}
+
+	@Bean
+	public S3Client s3Client(AwsCredentialsProvider credentialsProvider) {
+		return S3Client.builder()
+			.credentialsProvider(credentialsProvider)
+			.region(Region.AP_NORTHEAST_2)
+			.build();
+	}
+}
