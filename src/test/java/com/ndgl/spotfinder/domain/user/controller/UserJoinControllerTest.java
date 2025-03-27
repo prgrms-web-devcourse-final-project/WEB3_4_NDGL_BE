f52d8b113870662ca394a26104bfb564b6c1effc
NRKim <ning9303@gmail.com>
2025-03-27 14:28:26 +0900

    feature 유저 로그인 기능 개발 #1

---
+++ src/test/java/com/ndgl/spotfinder/domain/user/controller/UserJoinControllerTest.java
@@ -0,0 +1,48 @@
+package com.ndgl.spotfinder.domain.user.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ndgl.spotfinder.domain.user.dto.UserJoinRequest;
+import com.ndgl.spotfinder.domain.user.entity.Oauth;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserJoinControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	@DisplayName("")
+	void join_success() throws Exception {
+		UserJoinRequest request = UserJoinRequest.builder()
+			.provider(Oauth.Provider.GOOGLE)
+			.identify("123456789")
+			.email("testman001@gmail.com")
+			.nickName("testman001")
+			.blogName("testblog001")
+			.build();
+
+		mockMvc.perform(post("/api/v1/users/join")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.code").value(200))
+			.andExpect(jsonPath("$.message").value("OK"));
+	}
+}
