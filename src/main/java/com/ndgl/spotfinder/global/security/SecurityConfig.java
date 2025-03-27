4a168a67aa176def904b3cac10a148f04ae010f5
han <tkrhktkdwk10@gmail.com>
2025-03-27 11:50:10 +0900

    feat : add S3Config #18
    # Conflicts:
    #	.gitignore
    #	src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java

--- src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java
+++ src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java
@@ -3,28 +3,26 @@ package com.ndgl.spotfinder.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorizeRequests ->
-				authorizeRequests
-					.requestMatchers("/h2-console/**")
-					.permitAll()
-					.requestMatchers("/api/v1/**")
-					.permitAll()
-			)
-			.headers(headers ->
-				headers.frameOptions(frameOptions ->
-					frameOptions.sameOrigin()
-				)
-			)
-			.csrf(
-				csrf -> csrf.disable()
-			);
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(
+				request -> request.anyRequest().permitAll())
+			.csrf(AbstractHttpConfigurer::disable)
+			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
 		return http.build();
 	}
+
 }
