6fcb4a9930872722c1665b6ec795fc8c2d00cb13
NRKim <ning9303@gmail.com>
2025-03-28 09:19:37 +0900

    fix 유저 로그인 기능 개발 #1

--- src/main/java/com/ndgl/spotfinder/domain/user/dto/UserJoinRequest.java
+++ src/main/java/com/ndgl/spotfinder/domain/user/dto/UserJoinRequest.java
@@ -3,6 +3,7 @@ package com.ndgl.spotfinder.domain.user.dto;
 import com.ndgl.spotfinder.domain.user.entity.Oauth;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,9 +22,17 @@ public class UserJoinRequest {
 
 	@NotNull(message = "nickName 값이 필요합니다.")
 	@Size(min = 2, max = 15, message = "닉네임은 15자 이하로 입력해주세요.")
+	@Pattern(
+		regexp = "^[가-힣a-zA-Z0-9]+$",
+		message = "닉네임은 영어, 한글, 숫자만 입력할 수 있습니다."
+	)
 	private final String nickName;
 
 	@NotNull(message = "blogName 값이 필요합니다.")
 	@Size(min = 2, max = 20, message = "블로그 명은 20자 이하로 입력해주세요.")
+	@Pattern(
+		regexp = "^[가-힣a-zA-Z0-9]+$",
+		message = "블로그 명은 영어, 한글, 숫자만 입력할 수 있습니다."
+	)
 	private final String blogName;
 }
