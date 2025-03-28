3ddd8c5cb7c5272e2a1a3c5beba7af355ee73cf3
han <tkrhktkdwk10@gmail.com>
2025-03-28 12:27:09 +0900

    feat : 환경에 따라 S3 경로 PROD/TEST로 구분지어 추가 #18

--- src/main/java/com/ndgl/spotfinder/global/util/Ut.java
+++ src/main/java/com/ndgl/spotfinder/global/util/Ut.java
@@ -2,10 +2,22 @@ package com.ndgl.spotfinder.global.util;
 
 import java.util.List;
 
+import org.springframework.core.env.Environment;
+
 public class Ut {
+
 	public static class list {
 		public static boolean hasValue(List<?> list) {
 			return list != null && !list.isEmpty();
 		}
+	}
+
+	public static boolean isTestMode(Environment environment) {
+		for (String profile : environment.getActiveProfiles()) {
+			if ("test".equals(profile)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
