6c8f3690e408ccabf744cf4dbdd0434071563c96
han <tkrhktkdwk10@gmail.com>
2025-03-28 11:07:21 +0900

    refactor : S3 이미지 업로드 및 출력 기능 리팩토링 #18

--- src/main/java/com/ndgl/spotfinder/global/aws/s3/S3Util.java
+++ src/main/java/com/ndgl/spotfinder/global/aws/s3/S3Util.java
@@ -2,21 +2,33 @@ package com.ndgl.spotfinder.global.aws.s3;
 
 import java.util.UUID;
 
+import org.springframework.web.multipart.MultipartFile;
 
 public class S3Util {
-    private S3Util(){}
 
-    // 사진 저장 path 설정
-    public static String buildS3Key(long id, String fileType) {
-        return  UUID.randomUUID().toString();
-    }
+	public static String extractKeyFromUrl(String url) {
+		int domainEndIndex = url.indexOf(".com/");
+		return url.substring(domainEndIndex + 5);
+	}
 
-    public static String extractObjectKeyFromUrl(String url) {
-        int domainEndIndex = url.indexOf(".com/");
-        if (domainEndIndex != -1) {
-            return url.substring(domainEndIndex + 5); // ".com/" 의 길이인 5를 더해줍니다
-        }
-        return null;
-    }
+	public static String buildS3Key(long id, MultipartFile file) {
+		String originalFilename = file.getOriginalFilename();
+		String extension = getExtension(originalFilename);
+
+		return String.format("post/%d/%s.%s", id, UUID.randomUUID(), extension); // 만일 확장되면 type으로 구분할 것
+	}
+
+	public static String getExtension(String filename) {
+		int lastDotIndex = filename.lastIndexOf('.');
+
+		if (lastDotIndex < 0)
+			return "";
+
+		return filename.substring(lastDotIndex + 1);
+	}
+
+	public static String generateS3Url(String bucketName, String region, String key) {
+		return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key);
+	}
 
 }
