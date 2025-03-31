7370116eac6fb6093fcca452d0a94b0dd2c29579
HSH02 <tkrhktkdwk10@gmail.com>
2025-03-31 11:25:24 +0900

    feat : image PresignedURL 업로드/조회 기능 구현, demo 코드 내장됨

--- src/main/java/com/ndgl/spotfinder/global/aws/s3/S3Util.java
+++ src/main/java/com/ndgl/spotfinder/global/aws/s3/S3Util.java
@@ -2,8 +2,6 @@ package com.ndgl.spotfinder.global.aws.s3;
 
 import java.util.UUID;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import com.ndgl.spotfinder.domain.image.type.ImageType;
 
 public class S3Util {
@@ -13,25 +11,12 @@ public class S3Util {
 		return url.substring(domainEndIndex + 5);
 	}
 
-	public static String buildS3Key(long id, MultipartFile file, ImageType type) {
-		String originalFilename = file.getOriginalFilename();
-		String extension = getExtension(originalFilename);
-		String rootFolder = "prod/" + type.name();
+	public static String buildS3Key(ImageType imageType, long id, String fileType) {
+		String fileName = UUID.randomUUID().toString() + "." + fileType;
 
-		return String.format("%s/%d/%s.%s", rootFolder, id, UUID.randomUUID(), extension);
-	}
-
-	public static String getExtension(String filename) {
-		int lastDotIndex = filename.lastIndexOf('.');
-
-		if (lastDotIndex < 0)
-			return "";
-
-		return filename.substring(lastDotIndex + 1);
-	}
-
-	public static String generateS3Url(String bucketName, String region, String key) {
-		return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, region, key);
+		return switch (imageType) {
+			case POST -> "posts/" + id + "/" + fileName;
+		};
 	}
 
 }
