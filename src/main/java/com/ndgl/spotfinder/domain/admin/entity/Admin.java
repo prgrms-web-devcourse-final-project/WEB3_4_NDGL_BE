cd42e7e1230364a8b63a4b5bfd77477c4fc370a2
LeeKyoWon <kskw20@naver.com>
2025-04-01 20:09:25 +0900

    feat: Admin 엔티티 추가

---
+++ src/main/java/com/ndgl/spotfinder/domain/admin/entity/Admin.java
@@ -0,0 +1,43 @@
+package com.ndgl.spotfinder.domain.admin.entity;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.ndgl.spotfinder.global.base.BaseTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Admin extends BaseTime {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(unique = true)
+	String username;
+
+	@Column(unique = true)
+	String password;
+
+	@Builder
+	private Admin(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+	}
+}
