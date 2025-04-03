b35cf72c253b21e4d7a28f1bafcf1bcd27cd58e7
류대현 <eogus1027@naver.com>
2025-04-03 16:38:26 +0900

    fix: 필요한 경우에만 조회 쿼리를 실행하도록 수정

--- src/main/java/com/ndgl/spotfinder/domain/post/entity/Post.java
+++ src/main/java/com/ndgl/spotfinder/domain/post/entity/Post.java
@@ -16,6 +16,7 @@ import com.ndgl.spotfinder.global.base.BaseTime;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -46,7 +47,7 @@ public class Post extends BaseTime {
 	@LastModifiedDate
 	private LocalDateTime updatedAt;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
 
--- src/main/java/com/ndgl/spotfinder/domain/post/repository/PostRepository.java
+++ src/main/java/com/ndgl/spotfinder/domain/post/repository/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,14 +13,17 @@ import com.ndgl.spotfinder.domain.post.entity.Post;
 import com.ndgl.spotfinder.domain.user.entity.User;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+	@EntityGraph(attributePaths = {"hashtags"})
 	Slice<Post> findByIdLessThanOrderByCreatedAtDesc(Long lastId, PageRequest pageRequest);
 
+	@EntityGraph(attributePaths = {"hashtags"})
 	Slice<Post> findByUserAndIdLessThanOrderByCreatedAtDesc(User user, Long lastId, PageRequest pageRequest);
 
 	@Query("SELECT p FROM Post p " +
 		   "JOIN Like l ON p.id = l.targetId AND l.targetType = 'POST' " +
 		   "WHERE l.user.id = :userId AND p.id < :lastId " +
 		   "ORDER BY p.createdAt DESC")
+	@EntityGraph(attributePaths = {"hashtags"})
 	Slice<Post> findLikedPostsByUser(@Param("userId") Long userId, @Param("lastId") Long lastId,
 		PageRequest pageRequest);
 
