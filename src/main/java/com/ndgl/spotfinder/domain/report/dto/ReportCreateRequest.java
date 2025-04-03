51dfb85d4df242df039cc9516facd889864eeecd
LeeKyoWon <kskw20@naver.com>
2025-04-03 16:23:12 +0900

    fix: 신고 사유 validation @NotNull -> @NotBlank 로 수정

--- src/main/java/com/ndgl/spotfinder/domain/report/dto/ReportCreateRequest.java
+++ src/main/java/com/ndgl/spotfinder/domain/report/dto/ReportCreateRequest.java
@@ -2,18 +2,15 @@ package com.ndgl.spotfinder.domain.report.dto;
 
 import com.ndgl.spotfinder.domain.report.entity.ReportType;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record ReportCreateRequest(
-	@Positive(message = "유저 id 는 양수만 가능합니다.")
-	long reportedUserId,
-
 	@NotNull
 	ReportType reportType,
 
-	@NotNull
+	@NotBlank
 	@Size(max = 100, message = "신고 사유는 최대 100자까지 가능합니다.")
 	String reason
 ) {
