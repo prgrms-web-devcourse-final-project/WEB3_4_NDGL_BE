9fe043700f9c780673958a470df1d1cb2ac0077a
NRKim <ning9303@gmail.com>
2025-04-03 16:14:25 +0900

    feature : 토큰 refresh 로직 초안 작성

--- src/main/java/com/ndgl/spotfinder/domain/auth/service/AuthService.java
+++ src/main/java/com/ndgl/spotfinder/domain/auth/service/AuthService.java
@@ -2,17 +2,29 @@ package com.ndgl.spotfinder.domain.auth.service;
 
 import org.springframework.stereotype.Service;
 
+import com.ndgl.spotfinder.global.exception.ErrorCode;
 import com.ndgl.spotfinder.global.security.jwt.TokenProvider;
+import com.ndgl.spotfinder.global.security.redis.entity.RefreshToken;
+import com.ndgl.spotfinder.global.security.redis.repository.RefreshTokenRepository;
 
 @Service
 public class AuthService {
 	private final TokenProvider tokenProvider;
+	private final RefreshTokenRepository refreshTokenRepository;
 
-	public AuthService(TokenProvider tokenProvider) {
+	public AuthService(TokenProvider tokenProvider, RefreshTokenRepository refreshTokenRepository) {
 		this.tokenProvider = tokenProvider;
+		this.refreshTokenRepository = refreshTokenRepository;
 	}
 
 	public boolean tokenStatusCheck(String accessToken) {
 		return tokenProvider.validateToken(accessToken);
+	}
+
+	public String getRefreshTokenFromRedis(String userId) {
+		RefreshToken refreshToken = refreshTokenRepository.findById("refreshToken:" + userId)
+			.orElseThrow(ErrorCode.MISSING_REFRESH_TOKEN::throwServiceException);
+
+		return refreshToken.getToken();
 	}
 }
