290d0b9322c17cb49e61746b3321d33bbf813ae4
HSH02 <tkrhktkdwk10@gmail.com>
2025-04-03 17:19:17 +0900

    refactor : 이미지 도메인 리팩토링 #47

--- src/main/java/com/ndgl/spotfinder/domain/image/controller/ImageViewController.java
+++ src/main/java/com/ndgl/spotfinder/domain/image/controller/ImageViewController.java
@@ -7,34 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ndgl.spotfinder.domain.image.type.ImageType;
-import com.ndgl.spotfinder.domain.post.service.PostService;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/view-example/images")
 @RequiredArgsConstructor
 @Profile("dev")
-@Slf4j
 public class ImageViewController {
-
-	private final PostService postService;  // 포스트 서비스 주입
-
-	/**
-	 * 이미지 업로드 예시 페이지
-	 */
-	@GetMapping("/upload")
-	public String showUploadPage(Model model) {
-		model.addAttribute("id", 9999L);
-		model.addAttribute("imageType", ImageType.POST);
-		model.addAttribute("pageTitle", "이미지 업로드");
-		model.addAttribute("maxImageCount", 10);
-		model.addAttribute("allowedTypes", "image/jpeg, image/png, image/gif, image/webp");
-		model.addAttribute("maxFileSize", 5);
-
-		return "example-upload";
-	}
 
 	/**
 	 * 새 포스트 생성 페이지 - 실제로 연동되는 업로드 예시
--- src/main/java/com/ndgl/spotfinder/domain/image/dto/PresignedUrlsResponse.java
+++ src/main/java/com/ndgl/spotfinder/domain/image/dto/PresignedUrlsResponse.java
@@ -3,7 +3,14 @@ package com.ndgl.spotfinder.domain.image.dto;
 import java.net.URL;
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record PresignedUrlsResponse(
+	@Schema(
+		description = "S3 등록 URL",
+		example = "[\"https://ndgl-spot-finder-aws.s3.ap-northeast-2.amazonaws.com/{imageType}/${referenceId}\", "
+			+ "\"https://ndgl-spot-finder-aws.s3.ap-northeast-2.amazonaws.com/{imageType}/${referenceId}\"]"
+	)
 	List<URL> presignedUrls
 ) {
 }
