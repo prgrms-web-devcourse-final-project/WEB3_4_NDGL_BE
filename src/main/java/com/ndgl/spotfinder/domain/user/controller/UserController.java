c9026ec57e8d842ff58a9d3058fff6af7c2883c6
NRKim <ning9303@gmail.com>
2025-04-03 17:24:45 +0900

    feature : logout 기능 개발 착수

--- src/main/java/com/ndgl/spotfinder/domain/user/controller/UserController.java
+++ src/main/java/com/ndgl/spotfinder/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ndgl.spotfinder.domain.user.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,10 @@ import com.ndgl.spotfinder.domain.user.dto.UserLoginResponse;
 import com.ndgl.spotfinder.domain.user.entity.Oauth;
 import com.ndgl.spotfinder.domain.user.service.UserJoinService;
 import com.ndgl.spotfinder.domain.user.service.UserLoginService;
+import com.ndgl.spotfinder.domain.user.service.UserService;
+import com.ndgl.spotfinder.global.exception.ErrorCode;
 import com.ndgl.spotfinder.global.rsdata.RsData;
+import com.ndgl.spotfinder.global.security.jwt.TokenProvider;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -24,10 +28,15 @@ public class UserController {
 
 	private final UserLoginService userLoginService;
 	private final UserJoinService userJoinService;
+	private final TokenProvider tokenProvider;
+	private final UserService userService;
 
-	public UserController(UserLoginService userLoginService, UserJoinService userJoinService) {
+	public UserController(UserLoginService userLoginService, UserJoinService userJoinService,
+		TokenProvider tokenProvider, UserService userService) {
 		this.userLoginService = userLoginService;
 		this.userJoinService = userJoinService;
+		this.tokenProvider = tokenProvider;
+		this.userService = userService;
 	}
 
 	@PostMapping("/join")
@@ -52,6 +61,19 @@ public class UserController {
 			response);
 
 		return new RsData<>(responseDto.getCode(), responseDto.getMessage(), responseDto);
+
+	}
+
+	@PostMapping("/logout")
+	public RsData<Void> logout(
+		@CookieValue(value = "accessToken", required = false) String accessToken,
+		HttpServletResponse response
+	) {
+		if (accessToken == null) {
+			ErrorCode.MISSING_ACCESS_TOKEN.throwServiceException();
+		}
+
+		String userId = tokenProvider.getEmail(accessToken);
 
 	}
 }
