f2040eafd3cdb25898f8d459e52d8ab4955e48ca
NRKim <ning9303@gmail.com>
2025-04-05 11:00:11 +0900

    feature : cookie에 samesite 설정 추가

--- src/main/java/com/ndgl/spotfinder/global/security/cookie/TokenCookieUtil.java
+++ src/main/java/com/ndgl/spotfinder/global/security/cookie/TokenCookieUtil.java
@@ -3,7 +3,6 @@ package com.ndgl.spotfinder.global.security.cookie;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
@@ -14,22 +13,38 @@ public class TokenCookieUtil {
 
 	public void setTokenCookies(HttpServletResponse response, String accessToken) {
 		//  accessToken을 쿠키에 넣자!
-		Cookie accessCookie = new Cookie("accessToken", accessToken);
-		accessCookie.setHttpOnly(true);
-		//accessCookie.setSecure(true);
-		accessCookie.setSecure(false);
-		accessCookie.setPath("/");
-		accessCookie.setMaxAge(validationTime.intValue() / 1000);// ms -> s로 변환
+		// Cookie accessCookie = new Cookie("accessToken", accessToken);
+		// accessCookie.setHttpOnly(true);
+		// accessCookie.setSecure(true);
+		// //accessCookie.setSecure(false);
+		// accessCookie.setPath("/");
+		// accessCookie.setMaxAge(validationTime.intValue() / 1000);// ms -> s로 변환
 
-		response.addCookie(accessCookie);  // accessToken정보 cookie에 등록
+		int maxAge = validationTime.intValue() / 1000;
+
+		//  samesite 설정
+		String cookieString = String.format(
+			"%s=%s; Max-Age=%d; Path=%s; HttpOnly; Secure; SameSite=None",
+			accessToken,
+			maxAge
+		);
+
+		response.addHeader("Set-Cookie", cookieString);
 	}
 
 	public void cleanTokenCookies(HttpServletResponse response, String cookieName) {
-		Cookie cookie = new Cookie(cookieName, null);
-		cookie.setMaxAge(0);
-		cookie.setPath("/");
-		cookie.setHttpOnly(true);  // HttpOnly 속성 추가
-		cookie.setSecure(true);  // Secure 속성 추가
-		response.addCookie(cookie);
+		// Cookie cookie = new Cookie(cookieName, null);
+		// cookie.setMaxAge(0);
+		// cookie.setPath("/");
+		// cookie.setHttpOnly(true);  // HttpOnly 속성 추가
+		// cookie.setSecure(true);  // Secure 속성 추가
+		// response.addCookie(cookie);
+
+		String cookieString = String.format(
+			"%s=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=None",
+			cookieName
+		);
+
+		response.addHeader("Set-Cookie", cookieString);
 	}
 }
