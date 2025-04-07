ba7e2e2da3bc7a151230a48778c594bf5f17b884
NRKim <ning9303@gmail.com>
2025-04-07 15:34:54 +0900

    fix : cookie 파일 롤백

--- src/main/java/com/ndgl/spotfinder/global/security/cookie/TokenCookieUtil.java
+++ src/main/java/com/ndgl/spotfinder/global/security/cookie/TokenCookieUtil.java
@@ -26,17 +26,13 @@ public class TokenCookieUtil {
 		int maxAge = validationTime.intValue() / 1000;
 
 		String secureFlag = secure ? "; Secure" : "";
-		String domainFlag = (domain != null && !domain.isBlank()) ? "; Domain=" + domain : "";
-
-		log.info("secureFlag: " + secureFlag);
-		log.info("domainFlag: " + domainFlag);
 
 		//  samesite 설정
 		String cookieString = String.format(
-			"accessToken=%s; Max-Age=%d; Path=/;%s%s HttpOnly; SameSite=%s",
+			"accessToken=%s; Max-Age=%d; Path=/; Domain=%s; HttpOnly%s; SameSite=%s",
 			accessToken,
 			maxAge,
-			domainFlag,
+			domain,
 			secureFlag,
 			sameSite
 		);
