cd33d25abd8b9294b49502ed95af286352caa0c3
LeeKyoWon <kskw20@naver.com>
2025-04-08 16:27:16 +0900

    feat: Elasticsearch에서 인기 검색어/게시물을 조회하기 위한 Config, Service 파일 추가

---
+++ src/main/java/com/ndgl/spotfinder/domain/popular/service/elasticsearch/ElasticsearchPopularService.java
@@ -0,0 +1,148 @@
+package com.ndgl.spotfinder.domain.popular.service.elasticsearch;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ndgl.spotfinder.domain.popular.dto.KeywordCount;
+import com.ndgl.spotfinder.domain.popular.dto.PostCount;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.aggregations.LongTermsBucket;
+import co.elastic.clients.elasticsearch._types.aggregations.StringTermsBucket;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.json.JsonData;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ElasticsearchPopularService {
+
+	private final ElasticsearchClient elasticsearchClient;
+
+	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+	private static final String KEYWORD_SEARCH_INDEX = "dev-search-";
+	private static final String POST_VIEW_INDEX = "dev-post-view-";
+
+	// 인기 검색어 Top N 조회
+	public List<KeywordCount> findTopKeywords(long startTime, long endTime, int size) throws IOException {
+		String[] indices = getIndicesForTimeRange(startTime, endTime, KEYWORD_SEARCH_INDEX);
+
+		for (String index : indices) {
+			log.info("Elasticsearch 조회할 검색 인덱스 : {}", index);
+		}
+
+		JsonData startTimeJson = JsonData.of(timestampToISO8601String(startTime));
+		JsonData endTimeJson = JsonData.of(timestampToISO8601String(endTime));
+		log.info("Elasticsearch 조회할 시간 범위 : {} ~ {}", startTimeJson.toString(), endTimeJson.toString());
+
+		SearchResponse<Void> response = elasticsearchClient.search(builder -> builder
+				.index(List.of(indices)) // 여러 인덱스 검색
+				.query(q -> q
+					.range(r -> r
+						.field("@timestamp")
+						.gte(startTimeJson)
+						.lte(endTimeJson)
+					)
+				)
+				.aggregations("top_keywords", a -> a
+					.terms(t -> t
+						.field("keyword.joined")
+						.size(size)
+					)
+				)
+				.size(0), // 집계 결과만 필요하므로 검색 결과는 불필요
+			Void.class
+		);
+
+		List<KeywordCount> result = new ArrayList<>();
+		List<StringTermsBucket> buckets = response.aggregations().get("top_keywords").sterms().buckets().array();
+
+		for(StringTermsBucket bucket : buckets) {
+			result.add(new KeywordCount(bucket.key().stringValue(), bucket.docCount()));
+		}
+
+		log.info("Elasticsearch 인기 검색어 조회 완료: {} 개 키워드", buckets.size());
+
+		return result;
+	}
+
+	// 인기 포스트 Top N 조회
+	public List<PostCount> findTopPosts(long startTime, long endTime, int size) throws IOException {
+		String[] indices = getIndicesForTimeRange(startTime, endTime, POST_VIEW_INDEX);
+
+		for (String index : indices) {
+			log.info("Elasticsearch 조회할 게시물 인덱스 : {}", index);
+		}
+
+		JsonData startTimeJson = JsonData.of(timestampToISO8601String(startTime));
+		JsonData endTimeJson = JsonData.of(timestampToISO8601String(endTime));
+		log.info("Elasticsearch 조회할 시간 범위 : {} ~ {}", startTimeJson.toString(), endTimeJson.toString());
+
+		SearchResponse<Void> response = elasticsearchClient.search(builder -> builder
+				.index(List.of(indices)) // 여러 인덱스 검색
+				.query(q -> q
+					.range(r -> r
+						.field("@timestamp")
+						.gte(startTimeJson)
+						.lte(endTimeJson)
+					)
+				)
+				.aggregations("top_posts", a -> a
+					.terms(t -> t
+						.field("postId")
+						.size(size)
+					)
+				)
+				.size(0), // 집계 결과만 필요하므로 검색 결과는 불필요
+			Void.class
+		);
+
+		List<PostCount> result = new ArrayList<>();
+		List<LongTermsBucket> buckets = response.aggregations().get("top_posts").lterms().buckets().array();
+
+		for(LongTermsBucket bucket : buckets) {
+			result.add(new PostCount(bucket.key(), bucket.docCount()));
+		}
+
+		log.info("Elasticsearch 인기 게시물 조회 완료: {} 개 키워드", buckets.size());
+
+		return result;
+	}
+
+	// startTime ~ endTime 사이에 날짜가 변경 된다면 2일에 걸친 인덱스 모두 활용해야 함
+	private String[] getIndicesForTimeRange(long startTime, long endTime, String indexPrefix) {
+		String startDateStr = timestampToCurrentDateString(startTime);
+		String endDateStr = timestampToCurrentDateString(endTime);
+
+		if (startDateStr.equals(endDateStr)) {
+			return new String[] {indexPrefix + startDateStr};
+		}
+
+		return new String[] {indexPrefix + startDateStr, indexPrefix + endDateStr};
+	}
+
+	// epoch 를 yyyy-MM-dd 날짜 문자열로 변경
+	private String timestampToCurrentDateString(long timeStamp) {
+		LocalDate date = Instant.ofEpochMilli(timeStamp)
+			.atZone(ZoneId.systemDefault())
+			.toLocalDate();
+		return date.format(DATE_FORMATTER);
+	}
+
+	// epoch 를 Elasticsearch에서 사용하는 ISO 8601 형식으로 변환
+	private String timestampToISO8601String(long timestamp) {
+		return Instant.ofEpochMilli(timestamp)
+			.atZone(ZoneId.systemDefault())
+			.toOffsetDateTime() // 없으면 Asia/Seoul 이 포함돼서 에러 발생
+			.toString();
+	}
+}
---
+++ src/main/java/com/ndgl/spotfinder/global/elk/ElasticsearchConfig.java
@@ -0,0 +1,41 @@
+package com.ndgl.spotfinder.global.elasticsearch;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+
+@Configuration
+@EnableScheduling
+public class ElasticsearchConfig {
+	@Value("${elasticsearch.host}")
+	private String elasticsearchHost;
+
+	@Value("${elasticsearch.port}")
+	private int elasticsearchPort;
+
+	@Bean
+	public RestClient restClient() {
+		return RestClient.builder(
+				new HttpHost(elasticsearchHost, elasticsearchPort))
+			.build();
+	}
+
+	@Bean
+	public ElasticsearchTransport elasticsearchTransport(RestClient restClient) {
+		return new RestClientTransport(restClient, new JacksonJsonpMapper());
+	}
+
+	@Bean
+	public ElasticsearchClient elasticsearchClient(ElasticsearchTransport transport) {
+		return new ElasticsearchClient(transport);
+	}
+}
+
