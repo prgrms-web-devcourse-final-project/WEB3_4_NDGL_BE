30d5cf73df7c7cd5e5d238614c8130bc1a54143f
LeeKyoWon <kskw20@naver.com>
2025-04-08 16:23:27 +0900

    feat: 사용자 로그인/로그아웃 로깅 AOP 추가

---
+++ src/main/java/com/ndgl/spotfinder/global/aspect/logging/UserAuthLoggingAspect.java
@@ -0,0 +1,54 @@
+package com.ndgl.spotfinder.global.aspect.logging;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ndgl.spotfinder.domain.user.dto.UserLoginResponse;
+import com.ndgl.spotfinder.global.common.util.IpAddressUtil;
+import com.ndgl.spotfinder.global.common.util.MaskingUtil;
+import com.ndgl.spotfinder.global.common.util.RequestUtil;
+import com.ndgl.spotfinder.global.rsdata.RsData;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class UserAuthLoggingAspect {
+
+	private final ObjectMapper objectMapper;
+
+	@Around("execution(* com.ndgl.spotfinder.domain.user.controller.UserController.processGoogleLogin(..))")
+	public Object logGoogleLogin(ProceedingJoinPoint joinPoint) throws Throwable {
+		Object result = joinPoint.proceed();
+
+		HttpServletRequest request = RequestUtil.getCurrentRequest();
+		if (request == null) return result;
+
+		if (result instanceof RsData<?> rsData && rsData.getCode() == 200) {
+			UserLoginResponse loginResponse = (UserLoginResponse)rsData.getData();
+
+			Map<String, Object> logMap = new HashMap<>();
+			logMap.put("event", "Login");
+			logMap.put("oauth", loginResponse.getProvider());
+			logMap.put("user", MaskingUtil.maskEmail(loginResponse.getEmail()));
+			// TODO: logMap.put("userId", loginResponse.getUserId());
+			logMap.put("ipAddress", IpAddressUtil.getClientIp(request));
+
+			log.info(objectMapper.writeValueAsString(logMap));
+		}
+		return result;
+	}
+
+	// TODO: 로그아웃 로깅 로직 구현 필요
+}
+
