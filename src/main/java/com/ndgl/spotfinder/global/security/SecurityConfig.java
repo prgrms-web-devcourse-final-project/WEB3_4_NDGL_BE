920268c7474c48ace570d5cc52a06c37d1de6fec
LeeKyoWon <kskw20@naver.com>
2025-04-08 16:23:31 +0900

    feat: 로그아웃한 사용자 정보를 로그로 남기기 위한 설정 추가

--- src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java
+++ src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java
@@ -10,10 +10,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ndgl.spotfinder.global.security.handler.CustomAccessDeniedHandler;
 import com.ndgl.spotfinder.global.security.handler.CustomAuthenticationEntryPoint;
+import com.ndgl.spotfinder.global.security.handler.CustomAuthenticationFailureHandler;
 import com.ndgl.spotfinder.global.security.handler.CustomAuthenticationSuccessHandler;
 import com.ndgl.spotfinder.global.security.handler.CustomLogoutHandler;
 import com.ndgl.spotfinder.global.security.handler.CustomLogoutSuccessHandler;
@@ -29,9 +32,11 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final ObjectMapper objectMapper;
+	private final JwtFilter jwtFilter;
 	private final TokenProvider tokenProvider;
 	private final AdminUserDetailsService adminUserDetailsService;
-	private final CustomAuthenticationSuccessHandler successHandler;
+	private final CustomAuthenticationSuccessHandler adminAuthSuccessHandler;
+	private final CustomAuthenticationFailureHandler adminAuthFailureHandler;
 	private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
 	private final CustomLogoutHandler customLogoutHandler;
 	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
@@ -47,7 +52,8 @@ public class SecurityConfig {
 			.formLogin(
 				form -> form
 					.loginProcessingUrl("/api/*/admin/login")
-					.successHandler(successHandler)
+					.successHandler(adminAuthSuccessHandler)
+					.failureHandler(adminAuthFailureHandler)
 			)
 			.logout(logout -> logout
 				.logoutUrl("/api/*/admin/logout")
@@ -106,8 +112,9 @@ public class SecurityConfig {
 					frameOptions.sameOrigin()
 				)
 			)
-			.addFilterBefore(new JwtFilter(tokenProvider),
-				org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(jwtFilter,
+				UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(jwtFilter, LogoutFilter.class)
 			.exceptionHandling(exceptionHandling -> {
 				exceptionHandling
 					.authenticationEntryPoint(customAuthenticationEntryPoint) // 401 에러
