f33fdff903022c845990f3e6b94da24d488016c9
NRKim <ning9303@gmail.com>
2025-04-08 16:02:02 +0900

    fix : 코드 리뷰 내용 반영.

--- src/main/java/com/ndgl/spotfinder/global/exception/ErrorCode.java
+++ src/main/java/com/ndgl/spotfinder/global/exception/ErrorCode.java
@@ -21,7 +21,7 @@ public enum ErrorCode {
 	CONFLICTED_BLOG_NAME(HttpStatus.CONFLICT, "이미 사용중인 블로그 명 입니다."),
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "대상 유저가 없습니다."),
 	NO_APPLIED_SOCIAL_PLATFORM(HttpStatus.BAD_REQUEST, "지원하지 않는 소셜 플랫폼입니다"),
-	SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "클라이언트 통신 에러"),
+	SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 통신 에러"),
 
 	POST_ACCESS_DENIED(HttpStatus.FORBIDDEN, "작성자만 수정 또는 삭제할 수 있습니다."),
 	POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 포스트입니다."),
--- src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java
+++ src/main/java/com/ndgl/spotfinder/global/security/SecurityConfig.java
@@ -1,11 +1,7 @@
 package com.ndgl.spotfinder.global.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ndgl.spotfinder.global.security.handler.*;
-import com.ndgl.spotfinder.global.security.jwt.JwtFilter;
-import com.ndgl.spotfinder.global.security.jwt.TokenProvider;
-import com.ndgl.spotfinder.global.security.jwt.service.AdminUserDetailsService;
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -17,136 +13,144 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ndgl.spotfinder.global.security.handler.CustomAccessDeniedHandler;
+import com.ndgl.spotfinder.global.security.handler.CustomAuthenticationEntryPoint;
+import com.ndgl.spotfinder.global.security.handler.CustomAuthenticationSuccessHandler;
+import com.ndgl.spotfinder.global.security.handler.CustomLogoutHandler;
+import com.ndgl.spotfinder.global.security.handler.CustomLogoutSuccessHandler;
+import com.ndgl.spotfinder.global.security.jwt.JwtFilter;
+import com.ndgl.spotfinder.global.security.jwt.TokenProvider;
+import com.ndgl.spotfinder.global.security.jwt.service.AdminUserDetailsService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableAspectJAutoProxy
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final ObjectMapper objectMapper;
-    private final TokenProvider tokenProvider;
-    private final AdminUserDetailsService adminUserDetailsService;
-    private final CustomAuthenticationSuccessHandler successHandler;
-    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
-    private final CustomLogoutHandler customLogoutHandler;
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
-    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+	private final ObjectMapper objectMapper;
+	private final TokenProvider tokenProvider;
+	private final AdminUserDetailsService adminUserDetailsService;
+	private final CustomAuthenticationSuccessHandler successHandler;
+	private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+	private final CustomLogoutHandler customLogoutHandler;
+	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
-    /*
-     * 일반 유저용 SecurityFilterChain
-     * */
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	/*
+	 * 일반 유저용 SecurityFilterChain
+	 * */
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .formLogin(
-                        form -> form
-                                .loginProcessingUrl("/api/*/admin/login")
-                                .successHandler(successHandler)
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/api/*/admin/logout")
-                        .addLogoutHandler(customLogoutHandler)
-                        .logoutSuccessHandler(customLogoutSuccessHandler)
-                        .clearAuthentication(true)
-                )
-                .userDetailsService(adminUserDetailsService)
-                .csrf(csrf -> csrf.disable())
-                .cors(
-                        cors -> cors.configurationSource(corsConfigurationSource())
-                )
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/",
-                                "/api/v1/users/join",
-                                "/login/callback",
-                                "oauth2/**",
-                                "/api/v1/users/google/login/process",
-                                "/api/v1/users",
-                                "/api/v1/users/logout",
-                                "/api/v1/users/resign",
-                                "/api/v1/users/info",
-                                "/api/v1/auth/status",
-                                "/api/v1/auth/token/refresh",
-                                "/api/v1/users/google/login/process",
-                                "/api/*/admin/login",
-                                "/api/*/admin/join",
-                                "/api/v1/dev/**"
-                        )
-                        .permitAll() // 로그인 경로는 모두 허용
-                        .requestMatchers(
-                                "/h2-console/**",
-                                "/error",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
-                        )
-                        .permitAll()
-                        .requestMatchers(HttpMethod.GET,
-                                "/api/v1/posts/**",
-                                "/api/v1/posts/*/comments",
-                                "/api/v1/posts/*/comments/*"
-                        )
-                        .permitAll()
-                        .requestMatchers(HttpMethod.POST,
-                                "/api/*/reports/posts/{id}",
-                                "/api/*/reports/comments/{id}"
-                        )
-                        .authenticated()
-                        .requestMatchers(
-                                "/api/*/admin/**",
-                                "/api/*/reports/**"
-                        )
-                        .hasAuthority("ROLE_ADMIN")
-                        .anyRequest()
-                        .authenticated()
-                )
-                .headers(headers ->
-                        headers.frameOptions(frameOptions ->
-                                frameOptions.sameOrigin()
-                        )
-                )
-                .addFilterBefore(new JwtFilter(tokenProvider),
-                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exceptionHandling -> {
-                    exceptionHandling
-                            .authenticationEntryPoint(customAuthenticationEntryPoint) // 401 에러
-                            .accessDeniedHandler(customAccessDeniedHandler); // 403 에러
-                });
+		http
+			.formLogin(
+				form -> form
+					.loginProcessingUrl("/api/*/admin/login")
+					.successHandler(successHandler)
+			)
+			.logout(logout -> logout
+				.logoutUrl("/api/*/admin/logout")
+				.addLogoutHandler(customLogoutHandler)
+				.logoutSuccessHandler(customLogoutSuccessHandler)
+				.clearAuthentication(true)
+			)
+			.userDetailsService(adminUserDetailsService)
+			.csrf(csrf -> csrf.disable())
+			.cors(
+				cors -> cors.configurationSource(corsConfigurationSource())
+			)
+			.sessionManagement(session ->
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(
+					"/api/v1/users/join",
+					"/login/callback",
+					"oauth2/**",
+					"/api/v1/users/google/login/process",
+					"/api/v1/users",
+					"/api/v1/users/logout",
+					"/api/v1/users/resign",
+					"/api/v1/users/info",
+					"/api/v1/auth/status",
+					"/api/v1/auth/token/refresh",
+					"/api/v1/users/google/login/process",
+					"/api/*/admin/login",
+					"/api/*/admin/join",
+					"/api/v1/dev/**"
+				)
+				.permitAll() // 로그인 경로는 모두 허용
+				.requestMatchers(
+					"/h2-console/**",
+					"/error",
+					"/swagger-ui/**",
+					"/v3/api-docs/**"
+				)
+				.permitAll()
+				.requestMatchers(HttpMethod.GET,
+					"/api/v1/posts/**",
+					"/api/v1/posts/*/comments",
+					"/api/v1/posts/*/comments/*"
+				)
+				.permitAll()
+				.requestMatchers(HttpMethod.POST,
+					"/api/*/reports/posts/{id}",
+					"/api/*/reports/comments/{id}"
+				)
+				.authenticated()
+				.requestMatchers(
+					"/api/*/admin/**",
+					"/api/*/reports/**"
+				)
+				.hasAuthority("ROLE_ADMIN")
+				.anyRequest()
+				.authenticated()
+			)
+			.headers(headers ->
+				headers.frameOptions(frameOptions ->
+					frameOptions.sameOrigin()
+				)
+			)
+			.addFilterBefore(new JwtFilter(tokenProvider),
+				org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
+			.exceptionHandling(exceptionHandling -> {
+				exceptionHandling
+					.authenticationEntryPoint(customAuthenticationEntryPoint) // 401 에러
+					.accessDeniedHandler(customAccessDeniedHandler); // 403 에러
+			});
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
 
-        // 허용할 오리진 설정
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:8080",
-                "https://localhost:8080",
-                "http://localhost:3000",
-                "https://localhost:3000",
-                "https://cdpn.io",
-                "https://api.ndgl.shop",
-                "https://www.ndgl.shop"
-        ));
+		// 허용할 오리진 설정
+		configuration.setAllowedOrigins(Arrays.asList(
+			"http://localhost:8080",
+			"https://localhost:8080",
+			"http://localhost:3000",
+			"https://localhost:3000",
+			"https://api.ndgl.shop",
+			"https://www.ndgl.shop"
+		));
 
-        // 허용할 HTTP 메서드 설정
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+		// 허용할 HTTP 메서드 설정
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
 
-        // 자격 증명 허용 설정 (쿠키 등)
-        configuration.setAllowCredentials(true);
+		// 자격 증명 허용 설정 (쿠키 등)
+		configuration.setAllowCredentials(true);
 
-        // 허용할 헤더 설정
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+		// 허용할 헤더 설정
+		configuration.setAllowedHeaders(Arrays.asList("*"));
 
-        // CORS 설정을 특정 경로에 적용
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+		// CORS 설정을 특정 경로에 적용
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
 
-        return source;
-    }
+		return source;
+	}
 }
