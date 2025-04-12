38ef6a04b4e8ebe6fe13c1b16ba5b3210f0d2b50
LeeKyoWon <kskw20@naver.com>
2025-04-12 13:01:48 +0900

    fix: 3분마다 실행되도록 스케줄링 주기 수정

--- src/main/java/com/ndgl/spotfinder/domain/popular/scheduler/PopularTrendsScheduler.java
+++ src/main/java/com/ndgl/spotfinder/domain/popular/scheduler/PopularTrendsScheduler.java
@@ -3,6 +3,7 @@ package com.ndgl.spotfinder.domain.popular.scheduler;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ndgl.spotfinder.domain.popular.dto.KeywordCountDto;
@@ -25,13 +26,13 @@ public class PopularTrendsScheduler {
 	private final PopularService popularService;
 
 	// TODO: 안정화 되면 가동 시작
-	// @Scheduled(cron = "0 17 14 * * *") // 매일 00:00부터 30분 간격으로 실행
+	@Scheduled(cron = "0 */3 * * * *") // 매일 00:00부터 30분 간격으로 실행
 	public void updatePopularTrends() {
 		log.info("인기 검색어 및 게시물 업데이트 스케줄러 시작: {}", LocalDateTime.now());
 
 		try {
 			long endTime = System.currentTimeMillis();
-			long startTime = endTime - (30 * 60 * 1000);
+			long startTime = endTime - (3 * 60 * 1000);
 
 			// Elasticsearch 에서 인기 검색어 / 인기 포스트 조회
 			List<KeywordCountDto> topKeywords = elasticsearchPopularService.findTopKeywords(startTime, endTime, TOP_SIZE);
@@ -51,7 +52,13 @@ public class PopularTrendsScheduler {
 			List<PostCountDto> postCountDtos = redisPopularService.getPopularPosts(10);
 
 			for(int i = 0; i< keywordCountDtos.size(); i++) {
-				log.info("{}위 - 인기 검색어 : {}, 인기 키워드 : {}", i+1, keywordCountDtos.get(i).keyword(), postCountDtos.get(i).postId());
+				log.info("{}위 - 인기 검색어 : {} -{}",
+					i+1, keywordCountDtos.get(i).keyword(), keywordCountDtos.get(i).count());
+			}
+
+			for(int i = 0; i< postCountDtos.size(); i++) {
+				log.info("{}위 - 인기 게시물 : {} -{}",
+					i+1, postCountDtos.get(i).postId(), postCountDtos.get(i).count());
 			}
 
 			log.info("인기 검색어 및 게시물 업데이트 완료");
