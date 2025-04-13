0f0849a14b77031b26b070a27cf08fbe2be94788
hyg0527 <149460377+hyg0527@users.noreply.github.com>
2025-04-13 15:41:01 +0900

    feat: 댓글 status 타입 추가 #132

--- src/main/java/com/ndgl/spotfinder/domain/comment/entity/PostComment.java
+++ src/main/java/com/ndgl/spotfinder/domain/comment/entity/PostComment.java
@@ -17,6 +17,8 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -62,6 +64,12 @@ public class PostComment extends BaseTime implements Likeable {
 	@Builder.Default
 	@Setter
 	private boolean pinned = false;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	@Builder.Default
+	@Setter
+	private PostCommentStatus status = PostCommentStatus.PUBLIC;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "parent_id")
