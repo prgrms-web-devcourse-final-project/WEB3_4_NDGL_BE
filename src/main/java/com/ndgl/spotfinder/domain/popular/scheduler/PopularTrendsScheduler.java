bcc9e8d7650e170d3d7c932c3e2a91287040cafc
LeeKyoWon <kskw20@naver.com>
2025-04-13 19:24:36 +0900

    fix: 인기 검색어 및 게시물 스케줄러 가동 OFF

--- src/main/java/com/ndgl/spotfinder/domain/popular/scheduler/PopularTrendsScheduler.java
+++ src/main/java/com/ndgl/spotfinder/domain/popular/scheduler/PopularTrendsScheduler.java
@@ -3,7 +3,6 @@ package com.ndgl.spotfinder.domain.popular.scheduler;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ndgl.spotfinder.domain.popular.dto.KeywordCountDto;
@@ -25,7 +24,7 @@ public class PopularTrendsScheduler {
 	private final RedisPopularService redisPopularService;
 	private final PopularService popularService;
 
-	@Scheduled(cron = "0 */30 * * * *") // 매 30분 간격으로 실행
+	// @Scheduled(cron = "0 */30 * * * *") // 매 30분 간격으로 실행
 	public void updatePopularTrends() {
 		log.info("인기 검색어 및 게시물 업데이트 스케줄러 시작: {}", LocalDateTime.now());
 
