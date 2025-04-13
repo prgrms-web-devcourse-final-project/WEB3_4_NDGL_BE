351c7b6b302cfb409fb98bb48a90ed9423e43e6a
LeeKyoWon <kskw20@naver.com>
2025-04-13 15:49:51 +0900

    fix: 인기 검색어 및 게시물 스케줄러 가동 ON

--- src/main/java/com/ndgl/spotfinder/domain/popular/scheduler/PopularTrendsScheduler.java
+++ src/main/java/com/ndgl/spotfinder/domain/popular/scheduler/PopularTrendsScheduler.java
@@ -3,6 +3,7 @@ package com.ndgl.spotfinder.domain.popular.scheduler;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ndgl.spotfinder.domain.popular.dto.KeywordCountDto;
@@ -24,14 +25,13 @@ public class PopularTrendsScheduler {
 	private final RedisPopularService redisPopularService;
 	private final PopularService popularService;
 
-	// TODO: 안정화 되면 가동 시작
-	// @Scheduled(cron = "0 */3 * * * *") // 매일 00:00부터 30분 간격으로 실행
+	@Scheduled(cron = "0 */30 * * * *") // 매 30분 간격으로 실행
 	public void updatePopularTrends() {
 		log.info("인기 검색어 및 게시물 업데이트 스케줄러 시작: {}", LocalDateTime.now());
 
 		try {
 			long endTime = System.currentTimeMillis();
-			long startTime = endTime - (3 * 60 * 1000);
+			long startTime = endTime - (30 * 60 * 1000);
 
 			// Elasticsearch 에서 인기 검색어 / 인기 포스트 조회
 			List<KeywordCountDto> topKeywords = elasticsearchPopularService.findTopKeywords(startTime, endTime, TOP_SIZE);
