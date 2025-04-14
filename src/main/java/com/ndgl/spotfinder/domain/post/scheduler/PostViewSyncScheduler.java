842d01d7dd1e4148efc766d36dd36dbe16b239c0
류대현 <eogus1027@naver.com>
2025-04-14 16:19:04 +0900

    fix: 조회 수 배치 스케줄러에 로그 추가

--- src/main/java/com/ndgl/spotfinder/domain/post/scheduler/PostViewSyncScheduler.java
+++ src/main/java/com/ndgl/spotfinder/domain/post/scheduler/PostViewSyncScheduler.java
@@ -9,7 +9,9 @@ import com.ndgl.spotfinder.domain.post.dto.PostViewCountDto;
 import com.ndgl.spotfinder.domain.post.service.PostViewCountService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PostViewSyncScheduler {
@@ -19,10 +21,12 @@ public class PostViewSyncScheduler {
 
 	@Scheduled(fixedRate = POST_VIEW_COUNT_SYNC_PERIOD)
 	public void syncViews() {
+		log.debug("조회 수 배치 스케줄러 작동");
 		List<PostViewCountDto> views = postViewCountService.collectPostViewCounts();
 
 		if (!views.isEmpty()) {
 			postViewCountService.applyToDatabase(views);
 		}
+		log.debug("조회 수 DB 반영 완료");
 	}
 }
