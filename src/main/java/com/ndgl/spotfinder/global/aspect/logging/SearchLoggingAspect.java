d2269bab0f9654076e440552c3954d8fd143220c
LeeKyoWon <kskw20@naver.com>
2025-04-14 20:23:01 +0900

    feat: 캐싱을 통한 한명의 연속적인 검색 로깅 방지 로직 적용

--- src/main/java/com/ndgl/spotfinder/global/aspect/logging/SearchLoggingAspect.java
+++ src/main/java/com/ndgl/spotfinder/global/aspect/logging/SearchLoggingAspect.java
@@ -1,16 +1,19 @@
 package com.ndgl.spotfinder.global.aspect.logging;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ndgl.spotfinder.global.common.util.IpAddressUtil;
 import com.ndgl.spotfinder.global.common.util.RequestUtil;
+import com.ndgl.spotfinder.global.exception.ErrorCode;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -22,34 +25,53 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class SearchLoggingAspect {
 
+	private final RedisTemplate<String, String> redisTemplate;
 	private final ObjectMapper objectMapper;
 
-	@Around("execution(* com.ndgl.spotfinder.domain.search.controller.PostSearchController.searchPosts(..))")
-	public Object logKeyWordSearch(ProceedingJoinPoint joinPoint) throws Throwable {
+	private static final String SEARCH_KEY_PREFIX = "searched:keyword:";
+	private static final Duration CACHE_DURATION_TIME = Duration.ofMinutes(1);
 
-		String keyword = null;
-
-		Object[] args = joinPoint.getArgs();
-		for (Object arg : args) {
-			if (arg instanceof String keywordArg) {
-				keyword = keywordArg;
-			}
-		}
-
-		Object result = joinPoint.proceed();
+	@Before(value = "execution(* com.ndgl.spotfinder.domain.search.service.PostSearchService.searchPosts(..)) && args(.., keyword)")
+	public void logKeyWordSearch(String keyword) {
 
 		HttpServletRequest request = RequestUtil.getCurrentRequest();
-		if (request == null) return result;
+		if (request == null) return;
 
+		String ip = IpAddressUtil.getClientIp(request);
+		String userAgent = request.getHeader("User-Agent");
+		String searchedKeyword = SEARCH_KEY_PREFIX + keyword + ":" + ip;
+
+		// 최근 검색 여부 확인 및 캐싱
+		if(isKeywordRecentlySearched(searchedKeyword)) {
+			return;
+		}
+
+		// 검색어 로깅
+		loggingSearchedKeyword(keyword, ip, userAgent);
+	}
+
+	private boolean isKeywordRecentlySearched(String searchedKeyword) {
+		Boolean isExist = redisTemplate.hasKey(searchedKeyword);
+		if (Boolean.TRUE.equals(isExist)) {
+			return true;
+		}
+
+		redisTemplate.opsForValue().set(searchedKeyword, "true", CACHE_DURATION_TIME);
+		return false;
+	}
+
+	private void loggingSearchedKeyword(String keyword, String ip, String userAgent) {
 		// 로그 데이터 구성
 		Map<String, Object> logMap = new HashMap<>();
 		logMap.put("keyword", keyword);
-		logMap.put("ipAddress", IpAddressUtil.getClientIp(request));
-		logMap.put("userAgent", request.getHeader("User-Agent"));
+		logMap.put("ipAddress", ip);
+		logMap.put("userAgent", userAgent);
 
 		// 로그 남기기
-		log.info(objectMapper.writeValueAsString(logMap));
-
-		return result;
+		try {
+			log.info(objectMapper.writeValueAsString(logMap));
+		} catch (JsonProcessingException e) {
+			ErrorCode.JSON_PROCESSING_EXCEPTION.throwServiceException(e);
+		}
 	}
 }
